fe254d50ff9892f75d8c2aa99aa1682b25f791c7
Neelectric <Neel.R@web.de>
2025-05-05 10:45:00 +0000

    fix again lol

--- experiments/olmo2_upload_results_hf.py
+++ experiments/olmo2_upload_results_hf.py
@@ -5,9 +5,6 @@ import os
 login()
 api = HfApi()
 
-# Login (you'll need to generate a token from your HF account settings)
-api.login()
-
 # Create a new dataset repository
 dataset_name = "Neelectric/OLMo-2-1124-7B-Instruct_ROME_causal-trace-results"
 api.create_repo(repo_id=dataset_name, repo_type="dataset")
